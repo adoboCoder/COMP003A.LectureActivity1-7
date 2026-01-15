57eeb10d69f4aae9a284e5fec33fea0b4f42fdd6
jonathanrodrigocruz <jonathanrodrigocruz@LEM319-INST>
2026-01-14 19:31:20 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
